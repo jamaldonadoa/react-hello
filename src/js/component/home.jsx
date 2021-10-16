b10d3265629509d484b7dcee82a0cda38f5cd800
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-10-16 01:36:26 +0000

    Listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,102 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-const Home = () => {
+export const Navbar = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100">
+			<div className="container w-75">
+				<div>
+					<a className="navbar-brand" href="#">
+						Start Bootstrap
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-toggle="collapse"
+						data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+				</div>
+				<div
+					className="collapse navbar-collapse d-flex flex-row-reverse"
+					id="navbarSupportedContent ">
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Services
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export const Jumbotron = () => {
+	return (
+		<div className="jumbotron bg-light mx-5 p-5 mb-3">
+			<h1 className="display-4">A warm welcome!</h1>
+			<p className="lead">
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				Tempora aperiam rerum adipisci non voluptates ratione deleniti
+				inventore quis voluptas reprehenderit animi saepe eaque
+				necessitatibus, itaque expedita vitae totam neque ad.
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Call to Action!
+				</a>
 			</p>
 		</div>
 	);
 };
 
-export default Home;
+export const Tarjeta = ({ imgUrl }) => {
+	return (
+		<div className="card col-2 mb-5">
+			<img className="card-img-top" src={imgUrl} alt="Card image cap" />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">
+					Some quick example text to build on the card title and make
+					up the bulk of the cards content.
+				</p>
+				<a href="#" className="btn btn-primary text-wrap">
+					Find Out More!
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export const Footer = () => {
+	return (
+		<div className="footer text-center py-3 bg-dark">
+			<p className="text-white">Copywrite @ Your Website 2019</p>
+		</div>
+	);
+};
+
+Tarjeta.propTypes = {
+	imgUrl: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,26 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Navbar, Jumbotron, Tarjeta, Footer } from "./component/home.jsx";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<div className="container w-90">
+				<Jumbotron />
+				<div className="d-flex justify-content-between mx-5">
+					<Tarjeta imgUrl="https://picsum.photos/id/203/500/325" />
+					<Tarjeta imgUrl="https://picsum.photos/id/1003/500/325" />
+					<Tarjeta imgUrl="https://picsum.photos/id/200/500/325" />
+					<Tarjeta imgUrl="https://picsum.photos/id/143/500/325" />
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+ReactDOM.render(<App />, document.querySelector("#app"));
